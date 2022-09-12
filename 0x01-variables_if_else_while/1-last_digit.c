fd12d8a2b35bf3ebf806f0a6a89fa4930a20f1ad
abeersani <aburi.ms@hotmail.com>
2022-09-12 11:54:13 -0700

    first

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,3 +1,19 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+
+* main - Entry point
+
+*
+
+* Return: Always 0 (Success)
+
+*/
+
 int main(void)
 
 {
@@ -9,6 +25,10 @@ int n;
 
 
 int l;
+
+
+
+char str[] = 'Last digit of';
 
 
 
