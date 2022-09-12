0b034bba3483af42a267125fa53772b10b306ffc
abeersani <aburi.ms@hotmail.com>
2022-09-12 06:20:22 -0700

    7. Intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long int b;
 long long int c;
 char d;
 float f;
-printf("size of a char:%d byte(s)\n", sizeof(a));
-printf("Size of an int:%d byte(s)\n", sizeof(b));
-printf("size of a long int:%d byte(s)\n", sizeof(c)),
-printf("size of a long long int:%d byte(s)\n", sizeof(d));
-printf("Size of a float:%d byte(s)\n", sizeof(f));
+printf("size of a char:%lu byte(s)\n", sizeof(d));
+printf("Size of an int:%lu byte(s)\n", sizeof(a));
+printf("size of a long int:%lu byte(s)\n", sizeof(b)),
+printf("size of a long long int:%lu byte(s)\n", sizeof(c));
+printf("Size of a float:%lu byte(s)\n", sizeof(f));
 return (0);
 }
