ea216478e6f4d69ff366fb7dbadcb9a8b612dac4
abeersani <aburi.ms@hotmail.com>
2022-09-15 06:28:22 -0700

    task 99

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -1,28 +1,15 @@
 #include <stdio.h>
-
 #include "main.h"
 
-
-
 /**
-
 * main - prints the largest of 3 integers
-
 * Return: 0
-
 */
 
-
-
 int main(void)
-
 {
-
         int a, b, c;
-
         int largest;
-
-
 
         a = 972;
 
@@ -30,16 +17,9 @@ int main(void)
 
         c = 0;
 
-
-
         largest = largest_number(a, b, c);
-
-
 
         printf("%d is the largest number\n", largest);
 
-
-
         return (0);
-
 }
