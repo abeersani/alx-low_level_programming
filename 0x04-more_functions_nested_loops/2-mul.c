f70e80bee75804fe7cdec5db2d2657535601ff60
abeersani <aburi.ms@hotmail.com>
2022-09-16 02:30:05 -0700

    first new

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+
+ * mul - multiplies 2 digits
+
+ * @a: the first digit
+
+ * @b: the second digit
+
+ * Return: the product
+
+ */
+
+int mul(int a, int b)
+
+{
+
+	return (a * b);
+
+}
