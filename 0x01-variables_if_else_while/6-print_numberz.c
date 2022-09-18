19e9b0939b3c6095a1a53e8601ac524c4c8071ca
abeersani <aburi.ms@hotmail.com>
2022-09-18 02:40:38 -0700

    oh

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
 
-/**
- * main - The main function
- * Return: 0 for success
- */
-int main(void)
-{
-	int n;
-	int n1;
 
-	i = 48;
-	n1 = 10;
-	while (i <= 57)
-		putchar(i++);
-	putchar(n1);
-	return (0);
+
+/**
+ *
+ *  * main - The main function
+ *
+ *   * Return: 0 for success
+ *
+ *    */
+
+int main(void)
+
+{
+
+		int i;
+
+			int nl;
+
+
+
+				i = 48;
+
+					nl = 10;
+
+						while (i <= 57)
+
+									putchar(i++);
+
+							putchar(nl);
+
+								return (0);
+
 }
