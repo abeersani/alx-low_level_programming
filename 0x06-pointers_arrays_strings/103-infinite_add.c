356fef3ff2e6ee1773f3f7eb57d43022e3665fd8
abeersani <aburi.ms@hotmail.com>
2022-09-22 03:19:36 -0700

    fix 89

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,35 +1,59 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_number - printing number with puts
+ * main - check the code
  *
- * @n: input int
+ * Return: Always 0.
  */
-void print_number(int n)
+int main(void)
 {
-	/*Declaring variables*/
-	int count = 0, pow = 1;
-	unsigned int num = n;
+        char *n = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
+        char *m = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
+        char r[100];
+        char r2[10];
+        char r3[11];
+        char *res;
 
-
-	if (n < 0) /*Evaluate this condition*/
-	{
-		_putchar('-');
-		num = -n;
-	}
-	while (n != 0)
-	{
-		n /= 10;
-		count++;
-	}
-	while (count > 1)
-	{
-		pow *= 10;
-		count--;
-	}
-	while (pow >= 1)
-	{
-		_putchar(num / pow % 10 + '0');
-		pow /= 10;
-	}
+        res = infinite_add(n, m, r, 100);
+        if (res == 0)
+        {
+                printf("Error\n");
+        }
+        else
+        {
+                printf("%s + %s = %s\n", n, m, res);
+        }
+        n = "1234567890";
+        m = "1";
+        res = infinite_add(n, m, r2, 10);
+        if (res == 0)
+        {
+                printf("Error\n");
+        }
+        else
+        {
+                printf("%s + %s = %s\n", n, m, res);
+        }
+        n = "999999999";
+        m = "1";
+        res = infinite_add(n, m, r2, 10);
+        if (res == 0)
+        {
+                printf("Error\n");
+        }
+        else
+        {
+                printf("%s + %s = %s\n", n, m, res);
+        }
+        res = infinite_add(n, m, r3, 11);
+        if (res == 0)
+        {
+                printf("Error\n");
+        }
+        else
+        {
+                printf("%s + %s = %s\n", n, m, res);
+        }
+        return (0);
 }
