ddf00b0566ebe0171eebec1564b74c155071ca86
abeersani <aburi.ms@hotmail.com>
2022-09-22 02:46:04 -0700

    fix1

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,21 +1,31 @@
 #include "main.h"
-#include <string.h>
-#include <stdio.h>
-
 /**
- * cap_string - capitalizes all words of a string.
- * @str: pointer for the string
- * Return: pointer for the capitalized string.
+ * cap_string - capitalizes all words of a string
+ * @s: input string.
+ * Return: the pointer to dest.
  */
-char *cap_string(char *str)
-{
-	int i;
-	int j;
-	char sep[] = " \t\n,;.!?\"(){}";
 
-	printf("%lu\n", sizeof(sep));
-	for (i = 0; *(str + i) != '\0'; i++)
-		for (j = 0; sep[j] != '\0'; j++)
-			;
-	return (str);
+
+char *cap_string(char *s)
+{
+int count = 0, i;
+int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+if (*(s + count) >= 97 && *(s + count) <= 122)
+*(s + count) = *(s + count) - 32;
+count++;
+while (*(s + count) != '\0')
+{
+for (i = 0; i < 13; i++)
+{
+if (*(s + count) == separators[i])
+{
+if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+*(s + (count + 1)) = *(s + (count + 1)) - 32;
+break;
+}
+}
+count++;
+}
+return (s);
 }
