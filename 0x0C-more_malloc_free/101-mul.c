4517741e6d078f247a37014f3ad200367fa8aa32
abeersani <aburi.ms@hotmail.com>
2022-10-06 07:04:33 -0700

    kjk

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -295,6 +295,30 @@ if (tens)
 }
 
 /**
+ *
+ * * main - the main function
+ *
+ * * @argc: arguments count
+ *
+ * * @argv: arguments vector
+ *
+ * * Return: 0 for success
+ *
+ * */
+
+int main(int argc, char *argv[])
+
+{
+
+	(void) argc;
+
+	(void) argv;
+
+	return (0);
+
+}
+
+/**
 
 * main - Multiplies two positive numbers.
 
