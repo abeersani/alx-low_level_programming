c423c735d3ebd1ab9c637c8d3e7641c383385191
abeersani <aburi.ms@hotmail.com>
2022-10-10 07:28:16 -0700

    as

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H
+#define _DOG_H
 
-/*
+/**
  * File: dog.h
  * Auth: Abeer
  * Desc: Header file that defines a new type struct dog.
@@ -15,19 +15,19 @@
  */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+		char *name;
+			float age;
+				char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 /**
  * dog_t - Typedef for struct dog
  */
 typedef struct dog dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
-#endi
+#endif
